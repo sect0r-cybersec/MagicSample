e4c2bb4bfe591482023bb37c236485248588612c
Sam Harrison <harrison.2004@outlook.com>
2025-02-08 18:33:17 +0000

    Stem splitter library requires python 3.12

--- octachop.py
+++ octachop.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python3.12
+
 ## I don't know what these do, but librosa needs these or it breaks! Dependency nightmare!
 import aifc, sunau
 
 import librosa.core, librosa.beat
 
+import numpy as np
+
+import audio-separator
+
 path = ("test_data/omen.wav")
 
 audio_data, sr = librosa.load(path, sr=22050)
-bpm, beats = librosa.beat.beat_track(y=audio_data, sr=sr)
-print(bpm)
+
+def detect_bpm(audio_data, sr):
+    bpm_numpy_array, beats = librosa.beat.beat_track(y=audio_data, sr=sr)
+    bpm = (np.round(bpm_numpy_array).astype(int))[0]
+    return bpm
