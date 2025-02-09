71e54ee05e74dc0dde3bfed1e08c3fd011e3bb6e
Sam Harrison <harrison.2004@outlook.com>
2025-02-09 02:39:22 +0000

    Added spleeter library

--- octachop.py
+++ octachop.py
@@ -1,13 +1,30 @@
-#!/usr/bin/env python3.12
+#!/usr/bin/env python3.10
+## Shebang for linux/unix systems
+
+"""
+Note:
+
+The current version of this script only works with Python 3.10.
+
+This is to do with a dependency issue within spleeter. Until that is fixed, please run
+the script with Python 3.10 
+"""
 
 ## I don't know what these do, but librosa needs these or it breaks! Dependency nightmare!
 import aifc, sunau
 
+## Required for manipulating audio files, beat detection
 import librosa.core, librosa.beat
 
+## librosa returns data from methods as numpy array objects, so numpy is needed to manipulate that data
 import numpy as np
 
-import audio-separator
+## Spleeter is Deezer's stem separation ML model.
+## I'm using a pretrained algorithm supplied with the library to separate stems.
+from spleeter.separator import Separator
+
+## Instantiate 5 stem separator object
+separator = Separator("spleeter:5stems")
 
 path = ("test_data/omen.wav")
 
@@ -17,3 +34,7 @@ def detect_bpm(audio_data, sr):
     bpm_numpy_array, beats = librosa.beat.beat_track(y=audio_data, sr=sr)
     bpm = (np.round(bpm_numpy_array).astype(int))[0]
     return bpm
+
+print(detect_bpm(audio_data, sr))
+
+separator.separate_to_file(path,"/")
