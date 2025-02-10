4f89dcb2da2147211014f42ec269140b6c2dec06
Sam Harrison <harrison.2004@outlook.com>
2025-02-10 03:10:02 +0000

    Making it so octachop doesnt export straight to wav, but instead brings the stems into a format readable ly librosa

--- octachop.py
+++ octachop.py
@@ -35,32 +35,38 @@ input_path = ("test_data/omen.wav")
 output_path =("test_output")
 
 ## Obtain sample rate of given audio file(s)
-sample_rate = librosia.get_samplerate(input_path)
+## sample_rate = librosa.get_samplerate(input_path)
 
-audio_data, sr = librosa.load(input_path, sr=sample_rate)
+audio_data, sample_rate = librosa.load(input_path)
 
-def detect_bpm(audio_data, sr):
-    bpm_numpy_array, beats = librosa.beat.beat_track(y=audio_data, sr=sr)
+def detect_bpm(audio_data, sample_rate):
+    bpm_numpy_array, beats = librosa.beat.beat_track(y=audio_data, sr=sample_rate)
     bpm = (np.round(bpm_numpy_array).astype(int))[0]
     return bpm
 
-print(detect_bpm(audio_data, sr))
+print(detect_bpm(audio_data, sample_rate))
 
 ## To prevent the module from recursively calling itself
-if __name__ == "__main__":
-    ## Instantiate 5 stem separator object
-    ## Multiprocessing must be switched off else it will not work with windows
-    separator = Separator("spleeter:5stems-16kHz", multiprocess=False)
-    ## separator.separate_to_file(input_path, output_path)
+## Instantiate 5 stem separator object
+## Multiprocessing must be switched off else it will not work with windows
+separator = Separator("spleeter:5stems-16kHz", multiprocess=False)
+## separator.separate_to_file(input_path, output_path)
 
-    audio_loader = AudioAdapter.default()
-    waveform, _ = audio_loader.load(input_path, sample_rate=sample_rate)
+audio_loader = AudioAdapter.default()
+waveform, _ = audio_loader.load(input_path, sample_rate=sample_rate)
 
-    ## Takes the waveform and translates it into a dictionary object with the component name (vocals, bass etc)
-    ## As the key and a numpy flp array as the value
-    waveform_dict = separator.separate(waveform)
+## Takes the waveform and translates it into a dictionary object with the component name (vocals, bass etc)
+## As the key and a numpy flp array as the value
+waveform_dict = separator.separate(waveform)
+    
 list_of_keys = list(waveform_dict.keys())
 for key in list_of_keys:
     print (key)
     value = waveform_dict[key]
-    sf.write("{output_path}/{key}.wav", value, sample_rate, subtype="PCM_24")
+    print(value.dtype)
+    export_path = ("{}/{}.wav".format(output_path, key))
+    with sf.SoundFile(export_path, "w", sample_rate, 1, subtype="PCM_24") as wav_file:
+        sf.write(wav_file, value, sample_rate, subtype="PCM_24")
+
+if __name__ == "__main__":
+    main()
