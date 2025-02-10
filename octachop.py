799335357584b659727c95d0d7365819ba1ef247
Sam Harrison <harrison.2004@outlook.com>
2025-02-10 20:24:03 +0000

    Changed to export to file instead of keeping waveforms in memory

--- octachop.py
+++ octachop.py
@@ -32,42 +32,21 @@ from spleeter.separator import Separator
 from spleeter.audio.adapter import AudioAdapter
 
 input_path = ("test_data/omen.wav")
-output_path =("test_output")
+output_path =("temp")
 
-## Obtain sample rate of given audio file(s)
-## sample_rate = librosa.get_samplerate(input_path)
-
-audio_data, sample_rate = librosa.load(input_path)
+audio_data, sample_rate = librosa.load(input_path, sr=None, mono=False)
 
 def detect_bpm(audio_data, sample_rate):
     bpm_numpy_array, beats = librosa.beat.beat_track(y=audio_data, sr=sample_rate)
     bpm = (np.round(bpm_numpy_array).astype(int))[0]
     return bpm
 
-print(detect_bpm(audio_data, sample_rate))
+def split_to_stems(input_path, output_path):
+    ## Instantiate 5 stem separator object
+    ## Multiprocessing must be switched off else it will not work with windows
+    separator = Separator("spleeter:5stems-16kHz", multiprocess=False)
 
-## To prevent the module from recursively calling itself
-## Instantiate 5 stem separator object
-## Multiprocessing must be switched off else it will not work with windows
-separator = Separator("spleeter:5stems-16kHz", multiprocess=False)
-## separator.separate_to_file(input_path, output_path)
+    ## Handles all audio data automatically, with the disadvantage of outputting sound files and not as arrays
+    separator.separate_to_file(input_path, output_path, filename_format = "{instrument}.{codec}")
 
-audio_loader = AudioAdapter.default()
-waveform, _ = audio_loader.load(input_path, sample_rate=sample_rate)
-
-## Takes the waveform and translates it into a dictionary object with the component name (vocals, bass etc)
-## As the key and a numpy flp array as the value
-waveform_dict = separator.separate(waveform)
-    
-list_of_keys = list(waveform_dict.keys())
-for key in list_of_keys:
-    print (key)
-    value = waveform_dict[key]
-    print(value.dtype)
-    value = np.float32(value)
-    export_path = ("{}/{}.wav".format(output_path, key))
-    with sf.SoundFile(export_path, "w", sample_rate, 1, subtype="PCM_24") as wav_file:
-        sf.write(wav_file, value, sample_rate, subtype="PCM_24")
-
-if __name__ == "__main__":
-    main()
+split_to_stems(input_path, output_path)
